fc4cbe05afa43a645e3b152f09b21b756ace2144
namaskaro64 <german.saratov@gmail.com>
2025-02-10 01:24:15 +0400

    new  story

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 interface TableItem {
   name: string;
   amount: number;
+  subname?: string;
   price: number;
   size?: string;
   totalAmount: number;
@@ -15,26 +16,53 @@ interface TableProps {
 const Table = (props: TableProps) => {
   const { data } = props;
   return (
-    <table className="table table-success table-striped table-bordered table-responsive">
-      <thead>
-        <tr>
-          <th>Наименование</th>
-          <th>Количество</th>
-          <th>Цена за 1 единицу</th>
-          <th>Общая сумма</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => (
-          <tr>
-            <td>{item.name}</td>
-            <td>{item.amount}</td>
-            <td>{item.price}</td>
-            <td>{item.totalAmount}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div>
+      {data.some((item) => item.subname) ? (
+        <table className="table table-success table-striped table-bordered table-responsive">
+          <thead>
+            <tr>
+              <th>Наименование автомобиля</th>
+              <th>Наименование детали</th>
+              <th>Количество</th>
+              <th>Цена за 1 единицу</th>
+              <th>Общая сумма</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.subname}</td>
+                <td>{item.amount}</td>
+                <td>{item.price}</td>
+                <td>{item.totalAmount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <table className="table table-success table-striped table-bordered table-responsive">
+          <thead>
+            <tr>
+              <th>Наименование</th>
+              <th>Количество</th>
+              <th>Цена за 1 единицу</th>
+              <th>Общая сумма</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.amount}</td>
+                <td>{item.price}</td>
+                <td>{item.totalAmount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 };
 
